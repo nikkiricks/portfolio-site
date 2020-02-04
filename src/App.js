40fa14ac020eef902ac345d43dfd93e5291dd388
nikkiricks <nikki.ricks@gmail.com>
2020-02-04 14:51:21 +1100

    adding react routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "Nikki Ricks",
+      headerLinks: [
+        { title: "Home", path: "/"},
+        { title: "About", path: "/about"},
+        { title: "Contact", path: "/contact"}
+      ],
+      home: {
+        title: "Be Curious",
+        subTitle: "Projects that I care about",
+        text: "Checkout my projects below:"
+      },
+      about: {
+        title: "About Me",
+        subTitle: " ",
+        text: " "
+      },
+      contact: {
+        title: "Let's Chat",
+        subTitle: " ",
+        text: " "
+      }
+    }
+  }
+
   render() {
     return (
       <div>
